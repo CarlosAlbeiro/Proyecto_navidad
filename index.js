c173d522bd5169b4a75091c728895c282062d721
CAGG <carlosalbeirogaleano@hotmail.com>
2023-12-04 21:13:30 -0500

    Modificacion demostrativa

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app=express();
 
 app.get('/', (req, res)=>{
-    res.send("Servidor en linea")
+    res.send("Servidor en lina nicolas")
 })
 app.listen(5000)
 console.log("servidor en:"+ 5000);
