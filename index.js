5e40a4201188b60e8691a0d08b36851dc6a010d3
CAGG <carlosalbeirogaleano@hotmail.com>
2023-12-04 21:34:56 -0500

    Modificacion bd

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from "express";
+import { config } from "dotenv";
+import pg from 'pg'
 
 const app=express();
-
+const pool= new pg.Pool({
+    connectionString: process.env.DATABASE_URL
+})
 app.get('/', (req, res)=>{
     res.send("Servidor en lina nicolas")
+})
+app.get('/ping', async (req, res)=>{
+    const response=await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
 })
 app.listen(5000)
 console.log("servidor en:"+ 5000);
