9c94ec3571c088fd4ce32166f0d5aa5cb9b3bfaf
CAGG <carlosalbeirogaleano@hotmail.com>
2023-12-04 21:10:14 -0500

    Modificacion de prueba

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app=express();
 
 app.get('/', (req, res)=>{
-    res.send("Hola servidor en linea")
+    res.send("Servidor en linea")
 })
 app.listen(5000)
 console.log("servidor en:"+ 5000);
