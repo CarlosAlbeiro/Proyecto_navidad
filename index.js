7003fdc676fb56bbf26ef9f66dc1eb7a232c6c3d
Carlos (Sena) <Desweb5@espar.local>
2023-12-13 09:49:09 -0500

    Commit con mas rutas

--- index.js
+++ index.js
@@ -13,5 +13,20 @@ app.get('/ping', async (req, res)=>{
     const response=await pool.query('SELECT NOW()')
     return res.json(response.rows[0])
 })
+
+app.get('/crear_deseos', async (req, res)=>{
+    res.send("Ruta para crear deseos")
+})
+
+app.get('/eliminar_deseos', async (req, res)=>{
+    res.send("Ruta para crear deseos")
+})
+app.get('/registra_usuarios', async (req, res)=>{
+    res.send("Registrar usuarios")
+})
+app.get('/iniciar_sesion', async (req, res)=>{
+    res.send("Login")
+})
+
 app.listen(5000)
 console.log("servidor en:"+ 5000);
