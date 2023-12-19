9053df37a3bb42245daad15ebcff3f97c1add1fe
Carlos (Sena) <Desweb5@espar.local>
2023-12-19 08:23:59 -0500

    Cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,33 +1,42 @@
 import express from "express";
 import { config } from "dotenv";
 import pg from 'pg'
+const cors = require('cors');
 
-const app=express();
+const app = express();
 
-const pool= new pg.Pool({
+// Configuración para permitir cualquier origen (CORS)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Permite cualquier origen
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos permitidos
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Encabezados permitidos
+    next();
+});
+
+const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL
 })
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Aun sirve el despliegue")
 })
-app.get('/ping', async (req, res)=>{
-    const response=await pool.query('SELECT NOW()')
+app.get('/ping', async (req, res) => {
+    const response = await pool.query('SELECT NOW()')
     return res.json(response.rows[0])
 })
 
-app.get('/crear_deseos', async (req, res)=>{
+app.get('/crear_deseos', async (req, res) => {
     return res.json("Ruta para crear deseos");
 })
 
-app.get('/eliminar_deseos', async (req, res)=>{
+app.get('/eliminar_deseos', async (req, res) => {
     return res.json("Ruta para eliminar deseos");
 })
-app.get('/registra_usuarios', async (req, res)=>{
+app.get('/registra_usuarios', async (req, res) => {
     return res.json("Ruta registrar usuarios");
 })
-app.get('/iniciar_sesion', async (req, res)=>{
+app.get('/iniciar_sesion', async (req, res) => {
     return res.json("login");
 })
 
 app.listen(5000)
-console.log("servidor en:"+ 5000);
+console.log("servidor en:" + 5000);
