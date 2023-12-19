f62867ff25bd199ce3bec35e840e5a4b6f63ebe0
Carlos (Sena) <Desweb5@espar.local>
2023-12-19 09:58:29 -0500

    Correccion

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,8 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    connectTimeout: 15000,
 });
 
 try {
