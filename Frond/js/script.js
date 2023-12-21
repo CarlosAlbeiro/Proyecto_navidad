01e0bfe5c78d3e28a3dbec8668f584e209ba4b37
Carlos (Sena) <Desweb5@espar.local>
2023-12-21 15:48:53 -0500

    CAMBIOS EN FROND

--- Frond/js/script.js
+++ Frond/js/script.js
@@ -5,21 +5,27 @@ function consulta_deseos() {
     success: function (data) {
       let contenedor = document.getElementById('list_deseos')
       console.log(data);
-      // data.forEach(deseos => {
-      //   console.log(deseos.name);
-      //   var nuevoDiv = document.createElement("div");
+      data.forEach(deseos => {
+        contenedor.innerHTML = "";
 
-      //   // Configurar propiedades del nuevo div (opcional)
-      //   nuevoDiv.textContent = deseos.name;
-      //   if (deseos.id == 1) {
-      //     nuevoDiv.className = "col-4 badge bg-primary m-2";
-      //   } else {
-      //     nuevoDiv.className = "col-4 badge bg-danger m-2";
-      //   }
+        $("#list_deseos").append(`
+        <div class="col-md-3 col-sm-6 col-12 d-flex justify-content-center">
+          <div class="badge bg-primary">
 
-      //   // Agregar el nuevo div al contenedor
-      //   contenedor.appendChild(nuevoDiv);
-      // });
+          <div class="badge rounded-pill bg-light text-dark mb-1">
+            <span>${deseos.usuarios}</span>
+          </div>
+          <br>
+            <div class="badge rounded-pill bg-light text-dark">
+              <span>${deseos.producto}</span>
+            </div>
+            <div class=" d-flex justify-content-center mt-1">
+              <img src="${deseos.imagen}" class=" d-flex justify-content-center rounded" width="100">
+            </div>
+          </div>
+        </div>
+        `);
+      });
       // Crear un nuevo elemento div
     },
     error: function (error) {
@@ -32,33 +38,52 @@ function consulta_deseos() {
   });
 }
 
-function desear(){
-  let usuario_form=document.getElementById('usuario').value;
-  let producto_form=document.getElementById('producto').value;
-  let descripcion_form=document.getElementById('descripcion').value;
-  let link_form=document.getElementById('link').value;
-  let imagen_form=document.getElementById('imagen').value;
- 
-  $.ajax({
-    type: "POST",
-    url: "https://proyecto-navidad.onrender.com/crear_regalo",
-    data: {
-      usuario: usuario_form,
-      producto: producto_form,
-      descripcion: descripcion_form,
-      link:link_form,
-      imagen:imagen_form
-    },
-    success: function (response) {
-      console.log("Deseo ->". response);
-      
-    },
-    error: function (error) {
-      let errorCode=JSON.parse(error);
-      console.error(errorCode);
-    }
-  });
-  
+function desear() {
+  let usuario_form = document.getElementById('usuario').value;
+  usuario_form = usuario_form.toUpperCase();
+  let producto_form = document.getElementById('producto').value;
+  let descripcion_form = document.getElementById('descripcion').value;
+  let link_form = document.getElementById('link').value;
+  let imagen_form = document.getElementById('imagen').value;
+
+  if (!usuario_form || !producto_form || !descripcion_form) {
+    const toastLiveExample = document.getElementById('error')
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastBootstrap.show()
+  } else {
+
+    $.ajax({
+      type: "POST",
+      url: "https://proyecto-navidad.onrender.com/crear_regalo",
+      data: {
+        usuario: usuario_form,
+        producto: producto_form,
+        descripcion: descripcion_form,
+        link: link_form,
+        imagen: imagen_form
+      },
+      success: function (response) {
+        console.log("Deseo ->".response);
+        document.getElementById('name_user').innerHTML = usuario_form;
+
+        const toastLiveExample = document.getElementById('liveToast')
+        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+        toastBootstrap.show()
+      },
+      error: function (error) {
+        let errorCode = JSON.parse(error);
+        console.error(errorCode);
+        document.getElementById('mensaje_error').innerHTML = "Error al registrar tu regalo.";
+
+        const toastLiveExample = document.getElementById('error')
+        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+        toastBootstrap.show()
+      }
+    });
+  }
+
+
+
 }
 
 consulta_deseos();
